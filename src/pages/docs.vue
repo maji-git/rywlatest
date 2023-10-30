39c5295b1201ae83d82d6e6812c1b72e426a098e
devy <60780448+SK-Fast@users.noreply.github.com>
2023-10-30 21:15:06 +0700

    WIP View Docs

--- src/pages/docs.vue
+++ src/pages/docs.vue
@@ -1,5 +1,22 @@
 <template>
-    <f7-page name="settings">
-      <f7-navbar title="เอกสาร"></f7-navbar>
-    </f7-page>
+  <f7-page name="settings">
+    <f7-navbar title="เอกสาร"></f7-navbar>
+
+    <f7-block>
+      <div class="grid grid-cols-2 grid-gap mb-3">
+        <div>
+          <img src="@/assets/docs-thumbnails/ปก-2566-724x1024.png" class="img-field rounded shadow">
+          <p>คู่มือนักเรียน ปีการศึกษา 2566</p>
+        </div>
+        <div>
+          <img src="@/assets/docs-thumbnails/ใบลา.jpg" class="img-field rounded shadow">
+          <p>ใบลา</p>
+        </div>
+        <div>
+          <img src="@/assets/docs-thumbnails/แบบฟอร์มแก้คะแนนพฤติกรรมมาโรงเรียนสาย.jpg" class="img-field rounded shadow">
+          <p>แบบฟอร์มแก้คะแนนพฤติกรรมมาโรงเรียนสาย</p>
+        </div>
+      </div>
+    </f7-block>
+  </f7-page>
 </template>
